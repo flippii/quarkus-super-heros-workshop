15da62118bf19b3ad841b1f0d5c4659c33293827
flippii <Knut.Genthe@gmx.de>
2020-01-25 16:50:52 +0100

    add rest-fight

---
+++ super-heros/rest-fight/src/main/java/io/quarkus/workshop/superheroes/fight/client/Villain.java
@@ -0,0 +1,31 @@
+package io.quarkus.workshop.superheroes.fight.client;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import javax.validation.constraints.NotNull;
+
+@Schema(description="The villain fighting against the hero")
+public class Villain {
+
+    @NotNull
+    public String name;
+
+    @NotNull
+    public int level;
+
+    @NotNull
+    public String picture;
+
+    public String powers;
+
+    @Override
+    public String toString() {
+        return "Villain{" +
+            "name='" + name + '\'' +
+            ", level=" + level +
+            ", picture='" + picture + '\'' +
+            ", powers='" + powers + '\'' +
+            '}';
+    }
+
+}
